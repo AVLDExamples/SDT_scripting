0f2dd315017727910823be7fac36431514d4893e
Schmalhorst Carsten MUN <carsten.schmalhorst@avl.com>
2022-11-14 13:43:11 +0100

    Updated the listings

--- live_scripting/9101_Intake_Manifold/IMPRESS/exploring_data.py
+++ live_scripting/9101_Intake_Manifold/IMPRESS/exploring_data.py
@@ -7,13 +7,25 @@ result_proj = result_projects[0]
 result_proj.models[0]
 
 
-#second page
+#2nd page
 casenode = result_proj["9101_Intake_Manifold"]["Case set 1"]["Case 1"]
-twoD = casenode[“/DOM1/2D_Results”]
-outflow_node = casenode[“/DOM_1/2D_Results/Total Outlet Massflow”]
+twoD = casenode["/DOM_1/2D_Results"]
+outflow_node = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
 outflow_node.values
 
-#third page
+#3rd page: Task
+inflow = casenode["/DOM_1/2D_Results/Total Inlet Massflow"]
+outflow = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
+outflow = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
+inflow = np.asarray(inflow.values[1])
+outflow = np.asarray(outflow.values[1])
+balance = inflow - outflow
+np.max(balance)
+np.min(balance)
+
+
+
+#4th page
 import os
 projectname = os.path.splitext(os.path.basename(project.filename))[0]
 modelname = sdt.live.active_model().name
@@ -22,15 +34,13 @@ modelname = sdt.live.active_model().name
 #some address parts may be left out --> all matching channels will be returned
 channels = sdt.results.get_channel(project_directory=dirname, project=projectname, model=modelname )#, case_set="", case="", channel_path="")
 
-
-
-#fourth page
+#5th page
 sumfol = casenode.insert_summary_folder("Summary")
-sumfol.insert_single_value(name="MyName", value=repr(100), title="MyName" ,data_type="DOUBLE", unit_str="lenght~mm")
+sumfol.insert_single_value(name="MyName", value=repr(100), title="MyName" ,data_type="DOUBLE", unit_str="length~mm")
 casenode.write_tree()
 casenode.release()
 
-#fifth page
+#6th page
 import numpy as np
 root_folder=casenode.insert_root_folder("Additional Curves")
 curve_folder = root_folder.insert_folder(name="curves", title="Several Curves")
@@ -39,6 +49,6 @@ x_vec.values = np.linspace(-np.pi, np.pi,360)
 y_vec = sdt.results.create_vector(name="coordinate", unit_str="power~kW", title="Coordinate")
 y_vec.values = np.sin(x_vec.values)
 curve_folder.insert_curve(x=x_vec, y=y_vec,name="New Curve")
-asenode.write_tree()
+casenode.write_tree()
 casenode.release()
 
--- live_scripting/9101_Intake_Manifold/IMPRESS/modifying.py
+++ live_scripting/9101_Intake_Manifold/IMPRESS/modifying.py
@@ -1,12 +1,13 @@
-#adding page and view
-mdl = sdt.live.active_model()
-rep = mdl.report
-neuS = rep.create_page(“neue Seite”)
-view = neuS.create_view(‘Neuer View’, viewtype=“View3d”)
-d3d = view.add_entity(type=“ColoredSurface”, data=dataref)
-d3d.color_mapping.scheme = “Magma”
 #getting dataref
 projectname = os.path.splitext(os.path.basename(project.filename))[0]
 modelname = mdl.name
-datataref = ( dirname, projectname, modelname, “Case Set 1”, “Case 1”, “/DomainName/3D Result Data/NameInTree[UnitInTree]”, None)
+dataref = ( dirname, projectname, modelname, "Case set 1", "Case 1", "/DOM 1/3D Result Data/Flow:Velocity", None)
+#adding page and view
+mdl = sdt.live.active_model()
+rep = mdl.report
+neuS = rep.create_page("neue Seite")
+view = neuS.create_view("Neuer View", viewtype="View3d")
+d3d = view.add_entity(type="ColoredSurface", data=dataref)
+d3d.color_mapping.scheme = "Magma"
+
 
