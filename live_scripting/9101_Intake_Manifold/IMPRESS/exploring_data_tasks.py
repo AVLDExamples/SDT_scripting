77e38d19bc0cc8a96ce0bbbe2535d204bfd65e54
Schmalhorst Carsten MUN <carsten.schmalhorst@avl.com>
2022-12-09 15:09:47 +0100

    Update - missing lines

--- live_scripting/9101_Intake_Manifold/IMPRESS/exploring_data_tasks.py
+++ live_scripting/9101_Intake_Manifold/IMPRESS/exploring_data_tasks.py
@@ -1,17 +1,17 @@
 import os
 import sdt.results
+import numpy as np
 dirname = os.path.dirname(project.filename)
 result_projects = sdt.results.get_projects(dirname) # lists all projects within dirname
 result_proj = result_projects[0]
+casenode = result_proj["9101_Intake_Manifold"]["Case set 1"]["Case 1"]
 
 
 #task 1
-import numpy as np
-inflow = casenode["/DOM_1/2D_Results/Total Inlet Massflow"]
-outflow = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
-outflow = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
-inflow = np.asarray(inflow.values[1])
-outflow = np.asarray(outflow.values[1])
+inflow_node = casenode["/DOM_1/2D_Results/Total Inlet Massflow"]
+outflow_node = casenode["/DOM_1/2D_Results/Total Outlet Massflow"]
+inflow = np.asarray(inflow_node.values[1])
+outflow = np.asarray(outflow_node.values[1])
 balance = inflow - outflow
 np.max(balance)
 np.min(balance)
@@ -24,6 +24,8 @@ sumfol.insert_single_value(name="Min Imbalance", value=repr(min_value), title="M
 root_folder = casenode.insert_root_folder("Additional Data")
 x_vec = sdt.results.create_vector(name="Iterations", unit_str="time_iteration~iterations", title="Iterations")
 y_vec = sdt.results.create_vector(name="Massflow Imbalance", title="Massflow Imbalance", unit_str="massflow~kg_s")
+
+x_values = np.asarray(inflow_node.values[0])
 x_vec.values = x_values
 y_vec.values = balance
 root_folder.insert_curve(x=x_vec, y=y_vec, name="Imbalance")
